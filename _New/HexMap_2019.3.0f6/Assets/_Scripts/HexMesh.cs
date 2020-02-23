1f63294fb1e3701c934bfc53fbb8d436721e844b
Arthur-Delacroix <373974021@qq.com>
2020-02-23 13:27:39 +0800

    完成第17章
    未校对

--- _New/HexMap_2019.3.0f6/Assets/_Scripts/HexMesh.cs
+++ _New/HexMap_2019.3.0f6/Assets/_Scripts/HexMesh.cs
@@ -72,6 +72,14 @@ public class HexMesh : MonoBehaviour
     {
         Vector3 center = cell.transform.localPosition;
 
+        //这里是经过系数计算之后得出的实际顶点位置，剩下的与其用来做颜色混合，这个区域填充单一颜色
+        Vector3 v1 = center + HexMetrics.GetFirstSolidCorner(direction);
+        Vector3 v2 = center + HexMetrics.GetSecondSolidCorner(direction);
+
+        //混合区域外顶点位置信息
+        Vector3 v3 = center + HexMetrics.GetFirstCorner(direction);
+        Vector3 v4 = center + HexMetrics.GetSecondCorner(direction);
+
         //包括中点，每次取出3个顶点，依次存放到链表中
         for (int i = 0; i < 6; i++)
         {
@@ -82,11 +90,14 @@ public class HexMesh : MonoBehaviour
             //);
 
             //新的创建三角面片的方法，因为新增了颜色混合区域，这里使用经过系数修正的外接圆半径，而非原始的外接圆半径
-            AddTriangle(
-                center,
-                center + HexMetrics.GetFirstSolidCorner(direction),
-                center + HexMetrics.GetSecondSolidCorner(direction)
-            );
+            //AddTriangle(
+            //    center,
+            //    center + HexMetrics.GetFirstSolidCorner(direction),
+            //    center + HexMetrics.GetSecondSolidCorner(direction)
+            //);
+
+            //这里提前计算好了除重点外的另外两个顶点位置，直接使用
+            AddTriangle(center, v1, v2);
 
             //将每个顶点的颜色信息赋值给mesh
             //AddTriangleColor(cell.color);
@@ -98,15 +109,29 @@ public class HexMesh : MonoBehaviour
             //Color edgeColor = (cell.color + neighbor.color) * 0.5f;
             //AddTriangleColor(cell.color, edgeColor, edgeColor);
 
+            //添加混合区域
+            AddQuad(v1, v2, v3, v4);
+
             HexCell prevNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
             HexCell neighbor = cell.GetNeighbor(direction) ?? cell;
             HexCell nextNeighbor = cell.GetNeighbor(direction.Next()) ?? cell;
 
-            AddTriangleColor(
+            //添加三角形区域顶点颜色信息
+            //AddTriangleColor(
+            //    cell.color,
+            //    (cell.color + prevNeighbor.color + neighbor.color) / 3f,
+            //    (cell.color + neighbor.color + nextNeighbor.color) / 3f
+            //);
+
+            AddTriangleColor(cell.color);
+
+            //混合区域顶点颜色赋值
+            AddQuadColor(
+                cell.color,
                 cell.color,
                 (cell.color + prevNeighbor.color + neighbor.color) / 3f,
                 (cell.color + neighbor.color + nextNeighbor.color) / 3f
-            );
+             );
         }
     }
 
@@ -116,6 +141,14 @@ public class HexMesh : MonoBehaviour
         colors.Add(c1);
         colors.Add(c2);
         colors.Add(c3);
+    }
+
+    //为三角形每个顶点赋值相同颜色
+    private void AddTriangleColor(Color c)
+    {
+        colors.Add(c);
+        colors.Add(c);
+        colors.Add(c);
     }
 
     //分别将 顶点的位置 和 顶点的顺序，存放在相应的链表中
@@ -128,5 +161,30 @@ public class HexMesh : MonoBehaviour
         triangles.Add(vertexIndex);
         triangles.Add(vertexIndex + 1);
         triangles.Add(vertexIndex + 2);
+    }
+
+    //梯形的颜色混合区域的顶点信息
+    void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
+    {
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        vertices.Add(v4);
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex + 2);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 2);
+        triangles.Add(vertexIndex + 3);
+    }
+
+    //颜色混合区域 顶点颜色信息
+    void AddQuadColor(Color c1, Color c2, Color c3, Color c4)
+    {
+        colors.Add(c1);
+        colors.Add(c2);
+        colors.Add(c3);
+        colors.Add(c4);
     }
 }
