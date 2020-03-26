7706021774231c1fdde5406241ec9cfc05748259
Arthur-Delacroix <373974021@qq.com>
2020-03-26 22:20:34 +0800

    完成了第25章
    完成了第25章。未校对

--- _New/HexMap_2019.3.0f6/Assets/_Scripts/HexMetrics.cs
+++ _New/HexMap_2019.3.0f6/Assets/_Scripts/HexMetrics.cs
@@ -64,4 +64,39 @@ public static class HexMetrics
         //return (corners[(int)direction] + corners[(int)direction + 1]) * 0.5f * blendFactor;
         return (corners[(int)direction] + corners[(int)direction + 1]) * blendFactor;
     }
+
+
+
+    //关于斜边梯度的部分
+
+    //两个高度差1，并且相邻的HexCell，它们之间的过渡台阶数量
+    public const int terracesPerSlope = 2;
+
+    //根据台阶数量计算出实际的两个HexCell的 横向的连接区域的宽度
+    public const int terraceSteps = terracesPerSlope * 2 + 1;
+
+    //水平方向上，过渡台阶每一个部分的宽度(此为一个比例值，用来做lerp计算)
+    //注意，过渡台阶的宽度分为两部分，斜边为1个单位宽度，平台边为一个单位宽度，这里球出来的是每个单位宽度占整个宽度的百分比
+    public const float horizontalTerraceStepSize = 1f / terraceSteps;
+
+    //垂直方向上，每一个台阶的高度，由于是2个过渡台阶加上自身的台阶，所以要+1
+    public const float verticalTerraceStepSize = 1f / (terracesPerSlope + 1);
+
+    //根据预设的台阶数量，计算出每个过渡台阶的宽度和高度
+    public static Vector3 TerraceLerp(Vector3 a, Vector3 b, int step)
+    {
+        float h = step * HexMetrics.horizontalTerraceStepSize;
+        a.x += (b.x - a.x) * h;
+        a.z += (b.z - a.z) * h;
+        float v = ((step + 1) / 2) * HexMetrics.verticalTerraceStepSize;
+        a.y += (b.y - a.y) * v;
+        return a;
+    }
+
+    //为过渡台阶着色
+    public static Color TerraceLerp(Color a, Color b, int step)
+    {
+        float h = step * HexMetrics.horizontalTerraceStepSize;
+        return Color.Lerp(a, b, h);
+    }
 }
