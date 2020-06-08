0f95ad5f477672c9012114671a7f322c10527350
Arthur-Delacroix <373974021@qq.com>
2020-06-08 23:25:01 +0800

    add breakable item scripts
    为可破坏的箱子添加了代码，当箱子破坏后会产生随机的碎片，并想随机位置移动，当存在一定时间后会渐隐，并销毁自身

--- RoguelikeGame/Assets/_Scripts/Breakable.cs
+++ RoguelikeGame/Assets/_Scripts/Breakable.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#pragma warning disable 649
+
 public class Breakable : MonoBehaviour
 {
-    [SerializeField] private GameObject[] BrokenPiece;
+    [SerializeField] private GameObject[] BrokenPieces;
+
+    //一个箱子破碎时最多产生多少个碎片
+    [SerializeField] private int maxPieces;
 
 
     void Start()
@@ -17,17 +22,28 @@ public class Breakable : MonoBehaviour
 
     }
 
+    //当人物冲刺时碰到箱子会触发
     private void OnTriggerEnter2D(Collider2D _other)
     {
         if (_other.tag == "Player")
         {
+            //判断人物是否处在冲刺状态
             if (PlayerController.ins.dashCounter > 0)
             {
                 Destroy(gameObject);
 
-                int randomPiece = Random.Range(0, BrokenPiece.Length);
+                //产生随机数量的碎片
+                int piecesToDrop = Random.Range(1, maxPieces);
 
-                Instantiate(BrokenPiece[randomPiece], transform.position, transform.rotation);
+                //根据随机产生的碎片数量进行循环生成碎片
+                for (int i = 0; i < piecesToDrop; i++)
+                {
+                    //随机从数组中获得碎片的外观
+                    int randomPiece = Random.Range(0, BrokenPieces.Length);
+
+                    Instantiate(BrokenPieces[randomPiece], transform.position, transform.rotation);
+                }
+
             }
         }
     }
