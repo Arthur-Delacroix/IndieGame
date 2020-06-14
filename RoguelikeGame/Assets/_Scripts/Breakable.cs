9e70f55979ee754f9c03e3472d2a690939db954b
Arthur-Delacroix <373974021@qq.com>
2020-06-14 17:24:39 +0800

    add layers
    为新增加的道具添加了layer，并且完善了相互遮挡的关系

--- RoguelikeGame/Assets/_Scripts/Breakable.cs
+++ RoguelikeGame/Assets/_Scripts/Breakable.cs
@@ -24,50 +24,58 @@ public class Breakable : MonoBehaviour
     //当箱子破碎时会播放的音效的索引
     //[SerializeField] private int breakSound;
 
+    private void Smash()
+    {
 
+        Destroy(gameObject);
+
+        AudioManager.ins.playSFX(0);
+
+        //产生随机数量的碎片
+        int piecesToDrop = Random.Range(1, maxPieces);
+
+        //根据随机产生的碎片数量进行循环生成碎片
+        for (int i = 0; i < piecesToDrop; i++)
+        {
+            //随机从数组中获得碎片的外观
+            int randomPiece = Random.Range(0, BrokenPieces.Length);
+
+            Instantiate(BrokenPieces[randomPiece], transform.position, transform.rotation);
+        }
+
+        //触发掉落物品
+        if (shouldDropItem)
+        {
+            //首先随机产生一个0到100的数值
+            float dropChance = Random.Range(0f, 100f);
+
+            //判断该数值是否在掉落几率内
+            if (dropChance < itemDropPercent)
+            {
+                //随机选择一个掉落物品
+                int randomItem = Random.Range(0, itemToDrop.Length);
+
+                //生成掉落物品
+                Instantiate(itemToDrop[randomItem], transform.position, transform.rotation);
+            }
+        }
+    }
 
     //当人物冲刺时碰到箱子会触发
     private void OnTriggerEnter2D(Collider2D _other)
     {
-        if (_other.tag == "Player")
+        if (_other.tag == "Player" || _other.tag == "PlayerBullet")
         {
             //判断人物是否处在冲刺状态
             if (PlayerController.ins.dashCounter > 0)
             {
-                Destroy(gameObject);
-
-                AudioManager.ins.playSFX(0);
-
-                //产生随机数量的碎片
-                int piecesToDrop = Random.Range(1, maxPieces);
-
-                //根据随机产生的碎片数量进行循环生成碎片
-                for (int i = 0; i < piecesToDrop; i++)
-                {
-                    //随机从数组中获得碎片的外观
-                    int randomPiece = Random.Range(0, BrokenPieces.Length);
-
-                    Instantiate(BrokenPieces[randomPiece], transform.position, transform.rotation);
-                }
-
-                //触发掉落物品
-                if (shouldDropItem)
-                {
-                    //首先随机产生一个0到100的数值
-                    float dropChance = Random.Range(0f, 100f);
-
-                    //判断该数值是否在掉落几率内
-                    if (dropChance < itemDropPercent)
-                    {
-                        //随机选择一个掉落物品
-                        int randomItem = Random.Range(0, itemToDrop.Length);
-
-                        //生成掉落物品
-                        Instantiate(itemToDrop[randomItem], transform.position, transform.rotation);
-                    }
-                }
-
+                Smash();
             }
+        }
+
+        if (_other.tag == "PlayerBullet")
+        {
+            Smash();
         }
     }
 }
