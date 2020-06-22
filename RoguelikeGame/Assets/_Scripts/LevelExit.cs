535f5c9dcae942a58935930ac0ee2651c41642fd
Arthur-Delacroix <373974021@qq.com>
2020-06-22 17:43:04 +0800

    add level exit
    添加了转入其他关卡的功能

---
+++ RoguelikeGame/Assets/_Scripts/LevelExit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#pragma warning disable 649
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] private string levelToLoad;
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
