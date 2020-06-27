10e66d5edb4f09b350b33ec3db7903876dc8c41b
Arthur-Delacroix <373974021@qq.com>
2020-06-27 22:47:35 +0800

    add victory scene
    添加了胜利的场景，玩家按下任意键可以跳转至初始界面

---
+++ RoguelikeGame/Assets/_Scripts/VictoryScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#pragma warning disable 649
+
+public class VictoryScreen : MonoBehaviour
+{
+    [SerializeField] private float waitForAnyKey = 2f;
+
+    [SerializeField] private GameObject anyKeyText;
+
+    [SerializeField] private string mainMenuScene;
+
+    private void Update()
+    {
+        if (waitForAnyKey > 0)
+        {
+            waitForAnyKey -= Time.deltaTime;
+
+            if (waitForAnyKey <= 0)
+            {
+                anyKeyText.SetActive(true);
+            }
+        }
+        else
+        {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene(mainMenuScene);
+            }
+        }
+    }
+}
