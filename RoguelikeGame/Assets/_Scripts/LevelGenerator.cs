2850db6b5ff4b3c9bb03fc5d8f41a8183e83fb68
Arthur-Delacroix <373974021@qq.com>
2020-07-01 17:07:01 +0800

    update level generator script
    关卡生成器可以创建基础的房间，并为初始房间添加颜色

--- RoguelikeGame/Assets/_Scripts/LevelGenerator.cs
+++ RoguelikeGame/Assets/_Scripts/LevelGenerator.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#pragma warning disable 649
+
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //创建房间的模板
+    [SerializeField] private GameObject layoutRoom;
+
+    //一个关卡内房间的总数
+    [SerializeField] private int distanceToEnd;
+
+    //关卡起始房间的颜色
+    [SerializeField] private Color startColor;
+
+    //关卡结束房间的颜色
+    [SerializeField] private Color endColor;
+
+    //创建房间的起始点
+    [SerializeField] private Transform generatorPoint;
+
+    private void Start()
     {
-        
+        Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
