6cb6b68ece89aba0f34a47774b4cdfd3b924c845
Arthur-Delacroix <373974021@qq.com>
2020-07-02 19:45:53 +0800

    update generator
    更新了房间生成器的代码，可以在初始位置随机一个方向移动，并定义了横向和纵向每次移动的距离

--- RoguelikeGame/Assets/_Scripts/LevelGenerator.cs
+++ RoguelikeGame/Assets/_Scripts/LevelGenerator.cs
@@ -21,9 +21,44 @@ public class LevelGenerator : MonoBehaviour
     //创建房间的起始点
     [SerializeField] private Transform generatorPoint;
 
+    //创建房间的方位
+    public enum Direction { up, right, down, left };
+
+    //生成下一个房间的方位
+    [SerializeField] private Direction selectedDirection;
+
+    //房间中心点横向之间的距离
+    [SerializeField] private float xOffset;
+    //房间中心点纵向之间的距离
+    [SerializeField] private float yOffset;
+
     private void Start()
     {
         Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+
+        //为下一个房间随机一个方位
+        selectedDirection = (Direction)Random.Range(0, 4);
+        MoveGenerationPoint();
+    }
+
+    //按偏移量移动房间的生成器的位置
+    public void MoveGenerationPoint()
+    {
+        switch (selectedDirection)
+        {
+            case Direction.up:
+                generatorPoint.position += new Vector3(0f, yOffset, 0f);
+                break;
+            case Direction.right:
+                generatorPoint.position += new Vector3(xOffset, 0f, 0f);
+                break;
+            case Direction.down:
+                generatorPoint.position += new Vector3(0f, yOffset * -1f, 0f);
+                break;
+            case Direction.left:
+                generatorPoint.position += new Vector3(xOffset * -1f, 0f, 0f);
+                break;
+        }
     }
 
 }
