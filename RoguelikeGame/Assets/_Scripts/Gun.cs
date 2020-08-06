b93310c31baac4b18d649f6be966556eacbf1e78
Arthur-Delacroix <373974021@qq.com>
2020-08-06 19:57:10 +0800

    update gun script
    修改了枪械发射子弹的一些逻辑，当鼠标一直按下的时候，也会村从射击间隔发射子弹

--- RoguelikeGame/Assets/_Scripts/Gun.cs
+++ RoguelikeGame/Assets/_Scripts/Gun.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#pragma warning disable 649
+
 public class Gun : MonoBehaviour
 {
     //子弹预置
@@ -21,33 +23,41 @@ public class Gun : MonoBehaviour
     {
         if (PlayerController.ins.canMove && !LevelManager.ins.isPaused)
         {
-            //人物开枪射击
-            if (Input.GetMouseButtonDown(0))
-            {
-                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
-
-                shotCounter = timeBetweenShots;
-
-                AudioManager.ins.playSFX(12);
-            }
-
-            //连续射击
-            //当鼠标一直按下的时候，shotCounter开始随时间变小，当小于等于0的时候就会触发连续射击
-            //每次射出子弹后，重新初始化shotCounter的值
-            //这样保证了玩家一直按下鼠标时候不会立刻触发连续射击，然后每次连续射击的最大射速和连点的最大射速相同
-            //连续射击和连点射击的最大射速，都是timeBetweenShots控制
-            if (Input.GetMouseButton(0))
+            if (shotCounter > 0)
             {
                 shotCounter -= Time.deltaTime;
+            }
+            else
+            {
 
-                if (shotCounter <= 0)
+                //人物开枪射击
+                if (Input.GetMouseButtonDown(0)|| Input.GetMouseButton(0))
                 {
                     Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
 
-                    AudioManager.ins.playSFX(12);
-
                     shotCounter = timeBetweenShots;
+
+                    AudioManager.ins.playSFX(12);
                 }
+
+                //连续射击
+                //当鼠标一直按下的时候，shotCounter开始随时间变小，当小于等于0的时候就会触发连续射击
+                //每次射出子弹后，重新初始化shotCounter的值
+                //这样保证了玩家一直按下鼠标时候不会立刻触发连续射击，然后每次连续射击的最大射速和连点的最大射速相同
+                //连续射击和连点射击的最大射速，都是timeBetweenShots控制
+                //if (Input.GetMouseButton(0))
+                //{
+                //    shotCounter -= Time.deltaTime;
+
+                //    if (shotCounter <= 0)
+                //    {
+                //        Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+
+                //        AudioManager.ins.playSFX(12);
+
+                //        shotCounter = timeBetweenShots;
+                //    }
+                //}
             }
         }
     }
