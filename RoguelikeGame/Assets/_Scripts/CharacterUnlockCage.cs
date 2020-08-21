b71bd61c754f28b2693789e36c7826d53e8142e1
Arthur-Delacroix <373974021@qq.com>
2020-08-21 15:46:45 +0800

    update switch character system
    完善了人物切换的系统，游戏开始的时候，会有一个随机的人物在笼子里，玩家可以打开这个笼子解锁人物

---
+++ RoguelikeGame/Assets/_Scripts/CharacterUnlockCage.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUnlockCage : MonoBehaviour
+{
+    private bool canUnlock;
+
+    public GameObject message;
+
+    public CharacterSelector[] characterSelects;
+
+    public CharacterSelector playerUnlock;
+
+    public SpriteRenderer cageSR;
+
+    private void Start()
+    {
+        playerUnlock = characterSelects[Random.Range(0, characterSelects.Length)];
+
+        cageSR.sprite = playerUnlock.playerToSpawn.bodySR.sprite;
+    }
+
+    private void Update()
+    {
+        if (canUnlock)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Instantiate(playerUnlock, transform.position, transform.rotation);
+
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.tag == "Player")
+        {
+            canUnlock = true;
+            message.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D _other)
+    {
+        if (_other.tag == "Player")
+        {
+            canUnlock = false;
+            message.SetActive(false);
+        }
+    }
+}
