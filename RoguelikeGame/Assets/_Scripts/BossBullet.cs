3b978c7f9859e7f1baa9fe61d285bd2b75729621
Arthur-Delacroix <373974021@qq.com>
2020-08-28 21:58:56 +0800

    add boss scripts
    添加了boss的基本动作序列元素，子弹脚本等，为之后boss各种行为做准备

---
+++ RoguelikeGame/Assets/_Scripts/BossBullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBullet : MonoBehaviour
+{
+    //子弹速度
+    [SerializeField] private float speed;
+
+    //子弹的目标位置
+    private Vector3 direction;
+
+    void Start()
+    {
+        //确定玩家的位置
+        //direction = PlayerController.ins.transform.position - transform.position;
+
+        //direction.Normalize();
+
+        direction = transform.right;
+    }
+
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+
+        if (!BossController.ins.gameObject.activeInHierarchy )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.tag == "Player")
+        {
+            PlayerHealthController.ins.DamagePlayer();
+        }
+
+        Destroy(gameObject);
+
+        AudioManager.ins.playSFX(4);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ RoguelikeGame/Assets/_Scripts/BossController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public static BossController ins;
+
+    public BossAction[] actions;
+
+    private void Awake()
+    {
+        ins = this;
+    }
+}
+
+[System.Serializable]
+public class BossAction
+{
+    [Header("Action")]
+    public float actionLength;
+
+    public bool shouldMove;
+    public bool shouldChaseOlayer;
+    public bool moveToPoint ;
+
+    public float moveSpeed;
+
+    public Transform targetPoint;
+
+    public bool shouldShoot;
+    public GameObject bossBullet;
+    public float shootSpeed;
+    public Transform[] shootPoints;
+}
